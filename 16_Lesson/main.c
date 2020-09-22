4831c9717e51771f410f70fd8d07d94ee1200f45
Gamze Efendioglu <gamze1087@gmail.com>
2020-09-22 14:02:58 +0300

    Adding Switch-case examples, Goto Statements

--- 16_Lesson/main.c
+++ 16_Lesson/main.c
@@ -1,27 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h>
-#include <time.h>
 #include <stdlib.h>
 
+#define SIZE 5 
+
+typedef enum 
+{
+	COLOR_BLACK,
+	COLOR_WHITE, 
+	COLOR_PURPLE,
+	COLOR_YELLOW,
+	COLOR_GREEN
+}Colors;
+
+void print_color(Colors color) {
+
+	switch (color)
+	{
+	case COLOR_BLACK  : printf("Black\n"); break;
+	case COLOR_WHITE  : printf("White\n"); break;
+	case COLOR_PURPLE : printf("Purple\n"); break;
+	case COLOR_YELLOW : printf("Yellow\n"); break;
+	case COLOR_GREEN  : printf("Green\n"); break;
+	default: printf("Invalid Color Selection\n"); break;
+	}
+}
 
 int main() {
-	
-	srand((unsigned)time(NULL));
 
-	for (int i = 0; i < 20; ++i)
-	{
-		switch (rand() % 7 + 1){
-		case 1: printf("Monday "); break;
-		case 2: printf("Tuesday "); break;
-		case 3: printf("Wednesday "); break;
-		case 4: printf("Thursday "); break;
-		case 5: printf("Friday "); break;
-		case 6: printf("Saturday "); break;
-		case 7: printf("Sunday "); break;
-		}
-		
-		_getch();
+	Colors color = COLOR_BLACK; 
+
+	while (SIZE != 0) {
+
+		printf("0 : Black   \n");
+		printf("1 : White   \n");
+		printf("2 : Purple  \n");
+		printf("3 : Yellow  \n");
+		printf("4 : Green   \n\n");
+		printf("What is the selected card ? (0/1/2/3/4): ");
+
+		scanf("%d", &color);
+		print_color(color);
+		putchar('\n');
 	}
 	
 }
--- 17_Lesson/main.c
+++ 17_Lesson/main.c
@@ -1,6 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#define SUCCESS 1
+#define ERROR   0
+
+int allocate_memory(void) {
+	//...
+	return 1;
+}
+
+int setup_interrupts(void) {
+	//...
+	return 1;
+}
+
+int setup_registers(void) {
+	//...
+	return 0;
+}
+
+void teardown_interrupts(void) {}
+void free_memory(void){}
+
+
+int init_device(void) {
+
+	if (allocate_memory() != SUCCESS) goto out1;
+	if (setup_interrupts() != SUCCESS) goto out2;
+	if (setup_registers() != SUCCESS) goto out3;
+	//...
+	return SUCCESS;
+out1:
+	teardown_interrupts();
+out2:
+	free_memory();
+out3:
+	return ERROR;
+}
 
 int main() {
-
+	if (init_device()) {
+		printf("The initialization is successful");
+	}
 }
