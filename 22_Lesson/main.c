5f471b7899847b99a976e1f77ef4de47428e4028
Gamze Efendioglu <gamze1087@gmail.com>
2020-10-04 13:04:51 +0300

    Pointers and Const Variables

---
+++ 22_Lesson/main.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	const int primes[100] = { 0 }; 
+	int n = 10;
+	++primes[n];  // primes dizinin deðiþtirilmesi engellendi.
+
+}
