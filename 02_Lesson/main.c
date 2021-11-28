5f144f9d927c7e441d49b31ee8d2d081f4141441
gefendioglu <gmz.efendioglu@gmail.com>
2021-11-28 23:56:06 +0000

    Functions

--- 02_Lesson/main.c
+++ 02_Lesson/main.c
@@ -1,4 +1,32 @@
+
+#define LIFESPAN
+
+/* LifeSpan*/
+#if defined(LIFESPAN)
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(){}
+int n_digit(int val) 
+{
+	int digit_cnt = 0;
+	if (val == 0)
+		return 1;
+
+	while (val!=0)
+	{
+		++digit_cnt;
+		val /= 10;
+	}
+
+	return digit_cnt;
+}
+
+int main()
+{
+	int digit = 0;
+	printf("enter a digit number = ");
+	scanf("%d", &digit);
+	printf("digit number = %d", n_digit(digit));
+}
+#endif
+
